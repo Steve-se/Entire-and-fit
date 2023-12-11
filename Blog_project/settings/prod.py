5b1d597a0923f6adce6fb11641ac3bca12273e67
Big Steve <ejemstephen@gmail.com>
2023-12-11 13:20:45 +0100

    configured db for the prod settings, and also intalled dj-tool for the db

--- Blog_project/settings/prod.py
+++ Blog_project/settings/prod.py
@@ -1,21 +1,21 @@
 from .base import *
-
+import dj_database_url
 
 DEBUG = True
 
 # Databases
-DATABASES = {
-    'default': {
-        'ENGINE': env('DATABASE_ENGINE', None),
-        'NAME': env('DATABASE_NAME', None),
-        'USER': env('DATABASE_USER', None),
-        'PASSWORD': env('DATABASE_PASSWORD', None),
-        'HOST': env('DATABASE_HOST', None),
-        'PORT': env('DATABASE_PORT', None),
-    },
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': env('DATABASE_ENGINE', None),
+#         'NAME': env('DATABASE_NAME', None),
+#         'USER': env('DATABASE_USER', None),
+#         'PASSWORD': env('DATABASE_PASSWORD', None),
+#         'HOST': env('DATABASE_HOST', None),
+#         'PORT': env('DATABASE_PORT', None),
+#     },
+# }
 
-# DATABASES['default'] = dj_database_url.parse("postgres://entireandfit_user:DrsQlVXQdn9OFEtzt9rprQSlpDcwc9b1@dpg-clrebtie9h4c73av1q6g-a.oregon-postgres.render.com/entireandfit")
+DATABASES= {'default': dj_database_url.parse(env('DATABASE_URL'))}
 
 
 # set admin
